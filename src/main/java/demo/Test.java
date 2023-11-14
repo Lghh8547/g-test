f1323496bea5e7bb8514d36520170e5797468a82
xuchaofan8860 <1572290630@qq.com>
2023-11-14 20:12:55 +0800

    1

---
+++ src/main/java/demo/Test.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ * @Author：许超凡
+ * @Package：demo
+ * @Project：g-test
+ * @name：test
+ * @Date：2023/11/14 20:11
+ * @Filename：test
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
