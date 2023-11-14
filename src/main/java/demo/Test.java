f99cccf59a525731fc34ec5ded8d16064b74a664
xuchaofan8860 <1572290630@qq.com>
2023-11-14 20:24:26 +0800

    1

---
+++ src/main/java/demo/Test.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ * @Author：许超凡
+ * @Package：demo
+ * @Project：g-test
+ * @name：Test
+ * @Date：2023/11/14 20:23
+ * @Filename：Test
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
